4249590216100bee39e6aa94eea1bd838183cddd
Yuri Belikov <ysb@reasoningmind.org>
2018-11-22 12:36:20 +0300

    save

--- js/ChartPanel.js
+++ js/ChartPanel.js
@@ -16,13 +16,43 @@ ChartPanel.prototype.draw = function (canvas, data)
 {
   var ctx = canvas.getContext("2d");
   ctx.beginPath();
-
   ctx.clearRect(0, Dashboard.HEADER_H, canvas.width, canvas.height - Dashboard.HEADER_H);
+  ctx.closePath();
 
-  ctx.lineWidth = 2;
-  ctx.strokeStyle = "blue";
-  ctx.rect(20, Dashboard.HEADER_H + 20, 100, 100);
+  this.drawAxisX(canvas);
+  this.drawAxisY(canvas);
+
+
+};
+
+/**
+ * @this {ChartPanel}
+ * @param {HTMLCanvasElement} canvas
+ */
+ChartPanel.prototype.drawAxisX = function (canvas)
+{
+  var ctx = canvas.getContext("2d");
+  ctx.beginPath();
+
+  ctx.lineWidth = 1;
+  ctx.strokeStyle = "white";
+  var y = canvas.height - 50;
+  ctx.moveTo(50, y);
+  ctx.lineTo(canvas.width - 100, y);
   ctx.stroke();
+
+  ctx.closePath();
+};
+
+/**
+ * @this {ChartPanel}
+ * @param {HTMLCanvasElement} canvas
+ */
+ChartPanel.prototype.drawAxisY = function (canvas)
+{
+  var ctx = canvas.getContext("2d");
+  ctx.beginPath();
+
 
   ctx.closePath();
 };
