b8c631e182b12fc795f14a03cd232391ff7b3c77
Bel <yuribelikov@gmail.com>
2018-11-24 13:57:53 +0300

    save

--- js/DataLoader.js
+++ js/DataLoader.js
@@ -71,6 +71,8 @@ DataLoader.prototype.recentDataReceived = function (csv)
         dataRow.sensorsData[dataHeaders[j]] = parseNumber(cells[j]);
 
       this.data.push(dataRow);
+      if (this.data.length >= DataLoader.DATA_SIZE)
+        break;
     }
 
     log("recentDataReceived, data.size: " + this.data.length);
