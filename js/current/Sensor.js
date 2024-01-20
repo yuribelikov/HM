f4293166fbf1b2e93a4052e7d460c85942aa7cfe
Yuri <yuribelikov@gmail.com>
2024-01-20 12:56:02 +0300

    Update Sensor.js

--- js/current/Sensor.js
+++ js/current/Sensor.js
@@ -22,7 +22,7 @@ function Sensor(x, y, w, h, name, label, period)
   /** @type {TextBox} */
   this.labelBox = new TextBox(0.01, 0.01, 0.7, 0.16, label, "#00FF00", "left");
   /** @type {TextBox} */
-  this.largeValueBox = new TextBox(0, 0.3, 0.8, 0.6, "?", "#FFFFFF", "center");
+  this.largeValueBox = new TextBox(0, 0.3, 0.9, 0.6, "?", "#FFFFFF", "center");
   /** @type {TextBox} */
   this.valueBox = new TextBox(0.82, 0.86, 0.17, 0.1, "?", "#FFFFFF", "right", (Dashboard.NARROW ? 28 : 11));
 }
